b2e2930744b7a5ed983cfd1c277f621710f9aabf
chirag <c.taneja09@gmail.com>
2024-12-25 23:31:02 +0530

    Budget repo

---
+++ codequest/src/main/java/com/example/chirag/codequest/repository/BadgeRepository.java
@@ -0,0 +1,7 @@
+package com.example.chirag.codequest.repository;
+
+import com.example.chirag.codequest.model.Badge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BadgeRepository extends JpaRepository<Badge,Long> {
+}
