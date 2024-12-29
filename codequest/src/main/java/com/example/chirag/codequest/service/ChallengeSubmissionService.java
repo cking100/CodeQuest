4768da3e9db5f5d657e5784c225322eef9340c50
chirag <c.taneja09@gmail.com>
2024-12-29 20:08:28 +0530

    Password Encoder

--- codequest/src/main/java/com/example/chirag/codequest/service/ChallengeSubmissionService.java
+++
@@ -1,8 +0,0 @@
-package com.example.chirag.codequest.service;
-
-import com.example.chirag.codequest.dto.SubmissionResult;
-
-public interface ChallengeSubmissionService {
-    SubmissionResult validateAndSubmitSolution(Long challengeId, Long userId, String solutionCode);
-}
-
