d3b86199d0b025de78a40b3e6a4661b772db4f0d
chirag <c.taneja09@gmail.com>
2025-01-02 21:57:37 +0530

    Badge

--- codequest/src/main/java/com/example/chirag/codequest/repository/BadgeRepository.java
+++ codequest/src/main/java/com/example/chirag/codequest/repository/BadgeRepository.java
@@ -2,6 +2,7 @@ package com.example.chirag.codequest.repository;
 
 import com.example.chirag.codequest.model.Badge;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 public interface BadgeRepository extends JpaRepository<Badge,Long> {
+    List<Badge> findByUserId(Long userId);
 }
