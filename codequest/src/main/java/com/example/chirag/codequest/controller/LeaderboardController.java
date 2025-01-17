006a9ddecb59292997b8efbb1c189e640e71ae4c
chirag <c.taneja09@gmail.com>
2025-01-17 21:44:54 +0530

    LeaderBoard Controller

--- codequest/src/main/java/com/example/chirag/codequest/controller/LeaderboardController.java
+++ codequest/src/main/java/com/example/chirag/codequest/controller/LeaderboardController.java
@@ -16,22 +16,11 @@ public class LeaderboardController {
         this.leaderboardService = leaderboardService;
     }
 
-    /**
-     * Get the top users on the leaderboard.
-     *
-     * @return List of leaderboard entries.
-     */
     @GetMapping
     public List<Leaderboard> getTopUsers() {
         return leaderboardService.getTopUsers();
     }
 
-    /**
-     * Get the leaderboard position of a specific user.
-     *
-     * @param userId ID of the user.
-     * @return Leaderboard entry of the user.
-     */
     @GetMapping("/{userId}")
     public Leaderboard getUserPosition(@PathVariable Long userId) {
         return leaderboardService.getUserPosition(userId);
